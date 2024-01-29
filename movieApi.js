07ec8c8b3b0ee16652276800f823e687342a2b97
lauCodx <lauphix1@gmail.com>
2024-01-29 13:16:17 +0100

    MovieStore

---
+++ movieApi.js
@@ -0,0 +1,11 @@
+class movieStore {
+    constructor(id, movieName, movieRented){
+        this.id = id;
+        this.aMovie = movieName;
+        this.movieRented = movieRented;
+
+        this.movies =[];
+    }
+
+
+}
